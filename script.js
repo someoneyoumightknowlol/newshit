b2487819496cb84b93df3bd8b703d3dd0e1422ba
someoneyoudontknow <156366540+someoneyoumightknowlol@users.noreply.github.com>
2024-01-14 06:37:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-document.addEventListener('mousemove', (e) => {
-    const x = (e.clientX / window.innerWidth - 0.5) * 2;
-    const y = (e.clientY / window.innerHeight - 0.5) * 2;
-    const container = document.getElementById('container');
-    
-    container.style.transform = `rotateX(${y * 30}deg) rotateY(${x * 30}deg)`;
-  });
-  
+document.getElementById('instagram-button').addEventListener('mouseenter', playHoverSound);
+document.getElementById('discord-button').addEventListener('mouseenter', playHoverSound);
+
+function playHoverSound() {
+    var audio = document.getElementById('hoverSound');
+    audio.currentTime = 0; // Rewind to the beginning (in case the sound is still playing)
+    audio.play().catch(function(error) {
+        console.error('Error playing sound:', error);
+    });
+}
